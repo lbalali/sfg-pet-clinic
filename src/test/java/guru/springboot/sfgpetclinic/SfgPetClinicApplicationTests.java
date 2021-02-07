40f851d699820ebed47373d35daf25d896779246
lbalali <lbalali@yahoo.com>
2021-02-07 11:32:44 -0500

    Initial commit

---
+++ src/test/java/guru/springboot/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springboot.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
